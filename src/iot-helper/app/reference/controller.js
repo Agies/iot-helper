ee9e76ae845bb5ef10441245e12f4d6864443495
Jerel R Hass <agies1@hotmail.com>
2015-07-29 11:31:02 -0400

    search components

--- src/iot-helper/app/reference/controller.js
+++ src/iot-helper/app/reference/controller.js
@@ -1,6 +1,14 @@
 (function(module) {
 	'use strict';
 	module.controller('reference', function($scope) {
+		$scope.filter = "";
+		$scope.items = [];
+		$scope.items.push({
+			title: "Resistor",
+			view: "app/reference/components/resistor.html"
+		});
+	});
+	module.controller('resistor', function($scope) {
 		
-	})
+	});
 })(angular.module('iot.controllers'))
